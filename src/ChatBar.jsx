5a71c6e50ae2558a7e111e7197027c4ff69426ee
mindyli1120 <mindyli1989@gmail.com>
2018-07-10 19:38:01 +0000

    Able to render state with messages, currentUsername, and add new message

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+    constructor(props) {
+        super(props);
+        // this.state = {
+        //     currentUser: {name: "Bob"},
+        //     MessageList: [{
+        //         username: "Bob",
+        //         content: "Has anyone seen my marbles?",
+        //     },
+        //     {
+        //         username: "Anonymous",
+        //         content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        //     }]
+        // }
+    }
     render() {
         return (
             <footer className="chatbar">
-                <input className="chatbar-username" placeholder="Your Name (Optional)" />
+                <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser.name} />
                 <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
             </footer>
         )
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,6 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Message from './Message.jsx';
 
-export default function MessageList(){
-    return <Message/>;
+
+export default function MessageList(props){
+    const messageItem = props.messages.map(msg => {
+        return <Message 
+        key={msg.id}
+        username={msg.username}
+        content={msg.content} />
+    })
+    return <section>{messageItem}</section>;
 }
